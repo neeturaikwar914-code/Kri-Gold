7554d374c6f797551d14538660e634f31720ce62
Krishna <neeturaikwar914@gmail.com>
2026-03-03 23:56:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs");
+
+const moviesRoutes = require("./routes/movies");
+const usersRoutes = require("./routes/users");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req,res)=>{
-  res.send("Backend Running 🚀");
+  res.send("OTT Backend Running 🚀");
 });
 
-app.get("/movies",(req,res)=>{
-  const data = JSON.parse(fs.readFileSync("./data/movies.json"));
-  res.json(data);
-});
-
-app.post("/add-movie",(req,res)=>{
-  const movies = JSON.parse(fs.readFileSync("./data/movies.json"));
-  const newMovie = { id:Date.now(), ...req.body };
-  movies.push(newMovie);
-  fs.writeFileSync("./data/movies.json", JSON.stringify(movies,null,2));
-  res.json({success:true});
-});
+app.use("/movies", moviesRoutes);
+app.use("/users", usersRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>console.log("Server Started"));
+app.listen(PORT, ()=>{
+  console.log("Server running on", PORT);
+});
